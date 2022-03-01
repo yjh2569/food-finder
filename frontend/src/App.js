be01fe10efc368751a3f36f54b093c85f5b7486c
dddalang4 <dddalang4@naver.com>
2022-03-01 16:14:01 +0900

    v1.0.0 업데이트 - 기본적인 기능 추가
    * react, react hook을 사용해 음식 종류, 취향에 따라 랜덤한 음식을 추천하는 기능을 추가했습니다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
 
-function App() {
-  const [message, setMessage] = useState([]);
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Recommend from './routes/Recommend';
+import "./App.css";
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((response) => {
-        return response.json();
-      }).then(function (data) {
-        setMessage(data);
-      })
-  })
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-        </ul>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/recommend" element={<Recommend/>} />
+          </Routes>  
+        </BrowserRouter>
+    );  
 }
 
 export default App;
