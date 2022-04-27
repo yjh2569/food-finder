e9c14007d2f6eb7ca105d9401f4bb0914be427a4
yjh2569 <70595250+yjh2569@users.noreply.github.com>
2022-04-27 23:28:18 +0900

    에러 페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Recommend from './routes/Recommend';
+import NotFound from './routes/NotFound';
 import "./App.css";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/recommend" element={<Recommend/>} />
+            <Route component={<NotFound/>} />
           </Routes>  
         </BrowserRouter>
     );  
---
+++ frontend/src/routes/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const Home = () => {
+
+    return (
+        <div className="notFound">
+            <h1>404 Error : 해당 페이지를 찾을 수 없습니다!</h1>
+        </div>
+    )
+}
+
+export default NotFound;
